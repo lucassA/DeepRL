605f5e5f9dab1472fd780b07e9a980e968ecd5a8
lucas <lucas.albarede@protonmail.com>
2023-07-26 15:02:57 +0200

    t1

--- scripts/eval.py
+++ scripts/eval.py
@@ -10,7 +10,7 @@ from envs.custom_envs.fullFieldVision import FullFieldVisionEnv
 from envs.custom_envs.spiralFieldVision import SpiralFieldVisionEnv
 
 
-def evaluate(env_, policy, neval=10, deterministic=True, mode_eval="metrics", verbose=False):
+def perform_evaluation(env_, policy, neval=10, deterministic=True, mode_eval="metrics", verbose=False):
     if mode_eval == "metrics":
         mean_reward, std_reward = evaluate_policy(model=policy, env=env_, n_eval_episodes=neval,
                                                   deterministic=deterministic)
@@ -42,34 +42,7 @@ def evaluate(env_, policy, neval=10, deterministic=True, mode_eval="metrics", ve
 
 #################
 
-if __name__ == '__main__':
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument("-env", "--environment", type=str,
-                           help="Type of environment: spiral, coord, field, direction",
-                           choices=["spiral", "coord", "field", "direction"], required=True)
-    argParser.add_argument("-pmodel", "--path_model", type=str, help="Filepath from which to load the model from",
-                           required=True)
-    argParser.add_argument("-pmap", "--path_map", type=str, help="Filepath in which to find the map to test on",
-                           required=True)
-    argParser.add_argument("-ap", "--agent_placement", type=str, help="Type of placement for the agent: static, random",
-                           choices=["static", "random"], default="static")
-    argParser.add_argument("-ep", "--enemy_placement", type=str,
-                           help="Type of placement for the enemy: static, moves, random",
-                           choices=["static", "moves", "random"], default="static")
-    argParser.add_argument("-meval", "--mode_eval", type=str,
-                           help="Type of evaluation, either visual or metric-based",
-                           choices=["visual", "metrics"], default="visual")
-    argParser.add_argument("-mf", "--mode_vision", type=str,
-                           help="For the field environment, defines if the vision is static or dynamic",
-                           choices=["static", "dynamic"], default="dynamic")
-    argParser.add_argument("-nbep", "--nb_episode", type=int,
-                           help="Numer of episodes to evaluate the model on",
-                           default=1)
-    argParser.add_argument("-v", "--verbose", type=bool,
-                           help="Activate the verbose mode or not. Verbose prints each step of the model, non-verbose prints the last one",
-                           default=False)
-    args = argParser.parse_args()
-
+def evaluate(args):
     env = None
     model = None
     if args.environment == "spiral":
@@ -85,9 +58,6 @@ if __name__ == '__main__':
         check_env(env, warn=False)
         model = DQN.load(args.pmodel, env)
     elif args.environment == "field":
-        policy_kwargs = dict(
-            features_extractor_kwargs=dict(features_dim=5),
-        )
         env = FullFieldVisionEnv(map_file=args.pmap, enemy_placement=args.enemy_placement,
                                  player_placement=args.agent_placement,
                                  opti=args.opti, mode_vision=args.mode_vision)
@@ -100,5 +70,5 @@ if __name__ == '__main__':
         check_env(env, warn=False)
         model = DQN.load(args.pmodel, env)
 
-    evaluate(env_=env, policy=model, neval=args.nb_episode, mode_eval=args.mode_eval, deterministic=True,
+    perform_evaluation(env_=env, policy=model, neval=args.nb_episode, mode_eval=args.mode_eval, deterministic=True,
              verbose=args.verbose)
--- scripts/train.py
+++ scripts/train.py
@@ -8,37 +8,8 @@ from envs.custom_envs.fullDirectionOnField import FullDirectionOnFieldEnv
 from envs.custom_envs.fullFieldVision import FullFieldVisionEnv
 from envs.custom_envs.spiralFieldVision import SpiralFieldVisionEnv
 
-if __name__ == '__main__':
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument("-env", "--environment", type=str,
-                           help="Type of environment: spiral, coord, field, direction",
-                           choices=["spiral", "coord", "field", "direction"], required=True)
-    argParser.add_argument("-pmodel", "--path_model", type=str, help="Filepath in which to save the model",
-                           required=True)
-    argParser.add_argument("-pmap", "--path_map", type=str, help="Filepath in which to find the map", required=True)
-    argParser.add_argument("-ap", "--agent_placement", type=str, help="Type of placement for the agent: static, random",
-                           choices=["static", "random"], default="static")
-    argParser.add_argument("-ep", "--enemy_placement", type=str,
-                           help="Type of placement for the enemy: static, moves, random",
-                           choices=["static", "moves", "random"], default="static")
-    argParser.add_argument("-lg", "--logs", type=bool, help="Boolean indicating if logs are required, True or False",
-                           choices=[True, False], default=False)
-    argParser.add_argument("-llr", "--learn_lr", type=float, help="Learning rate, default is 0.0001", default=0.0001)
-    argParser.add_argument("-lts", "--learn_timesteps", type=int,
-                           help="Number of timestep to train for, default is 1000000", default=1000000)
-    argParser.add_argument("-lls", "--learn_learnstart", type=int,
-                           help="Number of timestep before training, default is 50000", default=50000)
-    argParser.add_argument("-lef", "--learn_exploration", type=float,
-                           help="Fraction of total timesteps for which randomness of action is applied", default=0.20)
-    argParser.add_argument("-o", "--opti", type=bool,
-                           help="Defines if optimization is activated for training. Available only for the original maps within the project",
-                           default=True)
-    argParser.add_argument("-mf", "--mode_vision", type=str,
-                           help="For the field environment, defines if the vision is static or dynamic",
-                           choices=["static", "dynamic"], default="dynamic")
-    args = argParser.parse_args()
 
-    env = None
+def train(args):
     model = None
     if args.environment == "spiral":
         env = SpiralFieldVisionEnv(map_file=args.pmap, enemy_placement=args.enemy_placement,
@@ -71,10 +42,6 @@ if __name__ == '__main__':
         check_env(env, warn=False)
         model = DQN("MlpPolicy", env, buffer_size=1000000, verbose=1, learning_starts=args.lls, train_freq=4,
                     exploration_fraction=args.lef, learning_rate=args.llr)
-
-
-
-
 
     # Logger
     if args.logs:
